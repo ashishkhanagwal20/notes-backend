ea5024f9c42c2272593fec1b27d817c7207c19bc
ashishkhanagwal20 <ashishstudy20@gmail.com>
2023-12-05 21:31:29 +0530

    use env file

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
+
+const Note = require("./models/note");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
@@ -25,20 +30,28 @@ let notes = [
 
 app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
+// app.get("/api/notes/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const note = notes.find((note) => {
+//     return note.id === id;
+//   });
+//   if (note) {
+//     res.json(note);
+//   } else {
+//     res.status(404).end("404 Page not found");
+//   }
+// });
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
   });
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end("404 Page not found");
-  }
 });
 
 const generateId = () => {
@@ -47,19 +60,19 @@ const generateId = () => {
 };
 app.post("/api/notes", (request, response) => {
   const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
